ca721888663ec6a239b6f04d07d9c98e90abe7c7
FelixJentsch <FelixJentsch91@googlemail.com>
2023-12-01 15:56:52 +0100

    challenge solved

--- src/App.js
+++ src/App.js
@@ -2,5 +2,21 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div>
+      <Sum valueA={5} valueB={3} />
+      <Sum valueA={52} valueB={8} />
+      <Sum valueA={5623} valueB={2122} />
+      <Sum valueA={9521} valueB={10} />
+      <Sum valueA={76} valueB={34} />
+    </div>
+  );
+}
+
+function Sum({ valueA, valueB }) {
+  return (
+    <p>
+      {valueA} + {valueB} = {valueA + valueB}
+    </p>
+  );
 }
